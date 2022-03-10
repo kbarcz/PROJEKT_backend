9372dbc889baa1cdbb4ae94c02fb09605c39006f
kasia <mail@gmail.com>
2022-03-10 18:09:51 +0100

    url hi

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.shortcuts import HttpResponse
+
+def hello(request):
+    return HttpResponse('Hi there...')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', hello),
 ]
